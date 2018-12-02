412c2c9454034df538bc183e7ffbc249311ee3c8
Katherine Prevost <kat@hypatian.org>
2018-12-02 11:32:46 -0500

    Fix up usage strings.

--- day2/problem1/src/Problem1.scala
+++ day2/problem1/src/Problem1.scala
@@ -3,7 +3,7 @@ import scala.io.Source
 object Problem1 {
   def main(args: Array[String]): Unit = {
     if ( args.length != 1 ) {
-      println("Usage: mill day1.problem1.run FILENAME")
+      println("Usage: mill day2.problem1.run FILENAME")
       System.exit(-1)
     }
     val filename = args(0)
