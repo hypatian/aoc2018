3aad28aba40fabaca3bc9af65be2f21daddf4b92
Katherine Prevost <kat@hypatian.org>
2018-12-06 03:16:55 -0500

    A little cleanup and commentary

--- day6/src/Day6.scala
+++ day6/src/Day6.scala
@@ -4,39 +4,40 @@ import org.hypatian.demo.aoc.AoCBase
 
 object Day6 extends AoCBase(6) {
 
-  val data: Seq[(Int, Int, Int)] = for {
-    (Array(x, y), id) <-
-      rawData.toSeq.map(_.split(", ").map(_.toInt)).zipWithIndex
-  } yield (id, x, y)
+  val data: Seq[(Int, Int)] = for {
+    Array(x, y) <- rawData.toSeq.map(_.split(", ").map(_.toInt))
+  } yield (x, y)
 
-  val maxX = data.map((id, x, y) => x).max
-  val maxY = data.map((id, x, y) => y).max
+  val width = data.map((x, y) => x).max + 1
+  val height = data.map((x, y) => y).max + 1
 
   def dist(x1: Int, x2: Int, y1: Int, y2: Int) = (x1 - x2).abs + (y1 - y2).abs
 
   def problem1() = {
-    val field = IndexedSeq.tabulate(maxX + 2, maxY + 2) { (x, y) =>
-      val distIds = data.map((id, px, py) => (dist(px, x, py, y), id))
-      val minDist = distIds.map((d, id) => d).min
-      val minPoints = distIds.filter((d, id) => d == minDist).map((d, id) => id)
-      if ( minPoints.size > 1 ) -1 else minPoints(0)
+    val field = IndexedSeq.tabulate(width + 1, height + 1) { (x, y) =>
+      val dists = data.map((px, py) => (dist(px, x, py, y)))
+      val minDist = dists.min
+      dists.zipWithIndex.
+        filter((d, id) => d == minDist).map((d, id) => id) match {
+          case Seq(id) => id              // Single nearest index
+          case _ => -1
+        }
     }
     val finiteIds = (
-      data.map((id, x, y) => id).toSet --
-      field.map(_(0)) --
-      field.map(_(maxY + 1)) --
-      field(0) --
-      field(maxX + 1)
+      (0 until data.size).toSet --
+      field.map(_(0)) --                // slice y = 0
+      field.map(_(height)) --           // slice y = height
+      field(0) --                       // slice x = 0
+      field(width)                      // slice x = width
     )
     val areas =
-      field.flatten.filter(finiteIds(_)).groupBy(identity).
-        mapValues(_.count(_ => true)).map((id, area) => area)
+      field.flatten.filter(finiteIds(_)).groupBy(identity).values.map(_.size)
     print(areas.max)
   }
 
   def problem2() = {
-    val field = IndexedSeq.tabulate(maxX + 2, maxY + 2) { (x, y) =>
-      data.map((_, px, py) => dist(px, x, py, y)).sum < 10000
+    val field = IndexedSeq.tabulate(width, height) { (x, y) =>
+      data.map((px, py) => dist(px, x, py, y)).sum < 10000
     }
     print(field.flatten.count(identity))
   }
