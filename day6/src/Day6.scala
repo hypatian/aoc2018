51db4c21d9b075490004d2037daf641b2291d7dd
Katherine Prevost <kat@hypatian.org>
2018-12-06 03:46:20 -0500

    Some further cleanup.

--- day6/src/Day6.scala
+++ day6/src/Day6.scala
@@ -13,25 +13,24 @@ object Day6 extends AoCBase(6) {
 
   def dist(x1: Int, x2: Int, y1: Int, y2: Int) = (x1 - x2).abs + (y1 - y2).abs
 
+  implicit class IndexedSeq2DSlices[T](s: IndexedSeq[IndexedSeq[T]]) {
+    def col(x: Int): IndexedSeq[T] = s(x)
+    def row(y: Int): IndexedSeq[T] = s.map(_(y))
+  }
+
   def problem1() = {
     val field = IndexedSeq.tabulate(width + 1, height + 1) { (x, y) =>
-      val dists = data.map((px, py) => (dist(px, x, py, y)))
-      val minDist = dists.min
-      dists.zipWithIndex.
-        filter((d, id) => d == minDist).map((d, id) => id) match {
-          case Seq(id) => id              // Single nearest index
-          case _ => -1
-        }
+      data.map((px, py) => (dist(px, x, py, y))).zipWithIndex.sorted match {
+        case Seq((d1, _), (d2, _), _: _*) if d1 == d2 => -1
+        case Seq((_, id), _: _*) => id
+      }
     }
-    val finiteIds = (
-      (0 until data.size).toSet --
-      field.map(_(0)) --                // slice y = 0
-      field.map(_(height)) --           // slice y = height
-      field(0) --                       // slice x = 0
-      field(width)                      // slice x = width
-    )
+    val finiteIds =
+      (data.indices.toSet --
+        field.row(0) -- field.row(height) --
+        field.col(0) -- field.col(width))
     val areas =
-      field.flatten.filter(finiteIds(_)).groupBy(identity).values.map(_.size)
+      field.flatten.filter(finiteIds).groupBy(identity).values.map(_.size)
     print(areas.max)
   }
 
