b51f75957af3e5b5188c33134415aded783e89cf
Katherine Prevost <kat@hypatian.org>
2018-12-08 01:00:41 -0500

    Day 8 golf golf golf

--- day8/src/Day8.scala
+++ day8/src/Day8.scala
@@ -15,20 +15,16 @@ object Day8 extends AoCBase(8) {
       else metadata.map(i => children.lift(i - 1)).flatten.map(_.value).sum
   }
   object Node {
-    def parse(input: Seq[Int]): (Seq[Int], Node) = {
-      @tailrec def loop(n: Int, input: Seq[Int], nodes: Seq[Node]):
-          (Seq[Int], Seq[Node]) = n match {
-        case 0 => (input, nodes)
-        case n =>
-          val (remainder, node) = parse(input)
-          loop(n - 1, remainder, nodes :+ node)
-      }
-      val Seq(nChildren, nMetadata, rem1: _*) = input
-      val (rem2, children) = loop(nChildren, rem1, Seq.empty)
-      (rem2.drop(nMetadata), Node(children, rem2.take(nMetadata)))
+    def parse(n: Int, input: Seq[Int], nodes: Seq[Node]): (Seq[Int], Seq[Node]) = n match {
+      case 0 => (input, nodes)
+      case n =>
+        val Seq(nChildren, nMetadata, rem1: _*) = input
+        val (rem2, children) = parse(nChildren, rem1, Seq.empty)
+        parse(n - 1, rem2.drop(nMetadata),
+          nodes :+ Node(children, rem2.take(nMetadata)))
     }
-    def apply(input: Seq[Int]): Node = parse(input) match {
-      case (Seq(), node) => node
+    def apply(input: Seq[Int]): Node = parse(1, input, Seq.empty) match {
+      case (Seq(), Seq(node)) => node
     }
   }
 
