7e65c6acf9e25f6bfbff6b160de1438ecf158293
Katherine Prevost <kat@hypatian.org>
2018-12-09 03:20:22 -0500

    Added some comments to explain the technique more

--- day9/src/Day9.scala
+++ day9/src/Day9.scala
@@ -30,27 +30,36 @@ object Day9 extends AoCBase(9) {
     // [1] Chris Okasaki, "Simple and Efficient Purely Functional
     // Queues and Deques":
     // https://www.westpoint.edu/eecs/SiteAssets/SitePages/Faculty%20Publication%20Documents/Okasaki/jfp95queue.pdf
+
+    // The front list (head is the current value)
+    // Start with two items in it to avoid having to deal with the
+    // "there aren't two items in the whole collection" case later.
     var f: List[Int] = List(0, 1)
+
+    // The back list (first item is the item before the current value, etc.)
     var b: List[Int] = List()
+
     val score = IndexedSeq.fill(players)(0L)
+
+    // Since 0 and 1 are already in there, start with 2
     var n = 2
     while ( n <= m ) {
       if ( n % 23 != 0 ) {
-        // Check if we have enough to consume two things, else reverse
-        if ( f.tail == Nil ) {
-          f = f.head :: b.reverse
-          b = Nil
-        }
+        // This is where we don't have to deal with < 2 items total.
+        // Check if we have enough to consume two things from f, else reverse
+        if ( ! f.isDefinedAt(1) ) { f = f ++ b.reverse; b = Nil }
+        // Copy two things from the front to the back
         b = f(1) :: f(0) :: b
+        // Drop those things and throw the new value on the front
         f = n :: f.drop(2)
       } else {
-        // Check if we have enough to consume six things, else reverse
-        if ( ! b.isDefinedAt(6) ) {
-          b = b ++ f.reverse
-          f = Nil
-        }
+        // Check if we have enough to consume seven things from b, else reverse
+        if ( ! b.isDefinedAt(6) ) { b = b ++ f.reverse; f = Nil }
+        // Add n and the seventh thing from the back to the player's score
         score((n - 1) % players) += n + b(6)
+        // Copy six things from the back to the front
         f = b(5) :: b(4) :: b(3) :: b(2) :: b(1) :: b(0) :: f
+        // Drop seven things from the back
         b = b.drop(7)
       }
       n += 1
