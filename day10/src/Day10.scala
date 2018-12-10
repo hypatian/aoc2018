f88d5566819f27ea2e3bfb4b13164e655ebc5bea
Katherine Prevost <kat@hypatian.org>
2018-12-10 00:44:11 -0500

    A little cleanup

--- day10/src/Day10.scala
+++ day10/src/Day10.scala
@@ -8,7 +8,7 @@ object Day10 extends AoCBase(10) {
 
   case class Point(x: Int, y: Int) {
     def +(other: Point) = Point(x + other.x, y + other.y)
-    def <(other: Point) = x < other.x && y < other.y
+    def <=(other: Point) = x <= other.x && y <= other.y
   }
 
   val pat =
@@ -18,8 +18,8 @@ object Day10 extends AoCBase(10) {
       (Point(px.toInt, py.toInt), Point(vx.toInt, vy.toInt))
   })
 
-  val initPositions = data.map((p, v) => p)
-  val velocities = data.map((p, v) => v)
+  val initPositions = data.map((p, v) => p).toIndexedSeq
+  val velocities = data.map((p, v) => v).toIndexedSeq
 
   def range(ps: Seq[Point]): Point =
     Point(ps.map(_.x).max - ps.map(_.x).min, ps.map(_.y).max - ps.map(_.y).min)
@@ -29,29 +29,26 @@ object Day10 extends AoCBase(10) {
     val maxX = ps.map(_.x).max
     val minY = ps.map(_.y).min
     val maxY = ps.map(_.y).max
-    val pss = ps.toSet
+    println()
     for ( y <- minY to maxY ) {
       for ( x <- minX to maxX ) {
-        if ( pss(Point(x, y)) ) print("#") else print(".")
+        if ( ps contains Point(x, y) ) print("#") else print(".")
       }
       println()
     }
   }
 
-  var seconds = 0
+  var seconds = -1
 
   def problem1() = {
-    var ps = initPositions
-    println()
-    while ( true ) {
-      val newPs = ps.zip(velocities).map((p, v) => p + v)
-      if ( range(ps) < range(newPs) ) {
-        plot(ps)
-        return
-      }
-      ps = newPs
+    var points = initPositions
+    var oldPoints = points
+    while ( range(points) <= range(oldPoints) ) {
       seconds += 1
+      oldPoints = points
+      points = points.zip(velocities).map((p, v) => p + v)
     }
+    plot(oldPoints)
   }
 
   def problem2() = print(seconds)
