17b48f4301b3e8770023f20b21a919e482d0b286
Katherine Prevost <kat@hypatian.org>
2018-12-10 23:14:15 -0500

    Switched things up to be purely functional for day 10.

--- day10/src/Day10.scala
+++ day10/src/Day10.scala
@@ -3,12 +3,34 @@ package org.hypatian.demo.aoc2018
 import org.hypatian.demo.aoc.AoCBase
 import scala.annotation.tailrec
 import scala.collection.mutable.{Buffer, IndexedSeq}
+import scala.math.{max, min}
 
 object Day10 extends AoCBase(10) {
 
   case class Point(x: Int, y: Int) {
     def +(other: Point) = Point(x + other.x, y + other.y)
-    def <=(other: Point) = x <= other.x && y <= other.y
+    def *(scale: Int) = Point(x * scale, y * scale)
+  }
+  object Point {
+    val MaxValue: Point = Point(Int.MaxValue, Int.MaxValue)
+    val MinValue: Point = Point(Int.MinValue, Int.MinValue)
+  }
+
+  implicit class PointSeqOps(self: Seq[Point]) {
+    def height: Int = self.map(_.y).max - self.map(_.y).min
+    def range: (Point, Point) =
+      self.foldLeft((Point.MaxValue, Point.MinValue))({
+        case ((a, b), p) =>
+          (Point(min(a.x, p.x), min(a.y, p.y)),
+            Point(max(b.x, p.x), max(b.y, p.y)))
+      })
+    def plot: String = {
+      val (Point(minX, minY), Point(maxX, maxY)) = range
+      (minY to maxY).map({ y =>
+        (minX to maxX).map({ x =>
+          if ( self contains Point(x, y) ) '#' else '.' }).mkString
+      }).mkString("\n")
+    }
   }
 
   val pat =
@@ -18,39 +40,21 @@ object Day10 extends AoCBase(10) {
       (Point(px.toInt, py.toInt), Point(vx.toInt, vy.toInt))
   })
 
-  val initPositions = data.map((p, v) => p).toIndexedSeq
-  val velocities = data.map((p, v) => v).toIndexedSeq
-
-  def range(ps: Seq[Point]): Point =
-    Point(ps.map(_.x).max - ps.map(_.x).min, ps.map(_.y).max - ps.map(_.y).min)
-
-  def plot(ps: Seq[Point]) = {
-    val minX = ps.map(_.x).min
-    val maxX = ps.map(_.x).max
-    val minY = ps.map(_.y).min
-    val maxY = ps.map(_.y).max
-    println()
-    for ( y <- minY to maxY ) {
-      for ( x <- minX to maxX ) {
-        if ( ps contains Point(x, y) ) print("#") else print(".")
-      }
-      println()
-    }
-  }
-
-  var seconds = -1
+  val p0 = data.map((p, v) => p).toIndexedSeq
+  val v = data.map((p, v) => v).toIndexedSeq
 
   def problem1() = {
-    var points = initPositions
-    var oldPoints = points
-    while ( range(points) <= range(oldPoints) ) {
-      seconds += 1
-      oldPoints = points
-      points = points.zip(velocities).map((p, v) => p + v)
-    }
-    plot(oldPoints)
+    val Some(tGoal, pGoal) = Stream.from(0).map({ t =>
+      val pt: Seq[Point] = p0.zip(v.map(_ * t)).map(_ + _)
+      (t, pt.height, pt)
+    }).sliding(2).collectFirst({
+      case Stream((t1, h1, pt1), (t2, h2, pt2)) if h2 > h1 => (t1, pt1)
+    })
+    println()
+    println(pGoal.plot)
+    println(s"In $tGoal seconds")
   }
 
-  def problem2() = print(seconds)
+  def problem2() = () // Done by problem1
 
 }
