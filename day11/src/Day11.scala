5a991cac3f435392993d0e9aa6f30905dd1cb412
Katherine Prevost <kat@hypatian.org>
2018-12-11 13:57:41 -0500

    This is the more correct solution for day 11, albeit slower. I like the way mutable.Map can be used for memoizing here, though. Nice!

--- day11/src/Day11.scala
+++ day11/src/Day11.scala
@@ -1,37 +1,34 @@
 package org.hypatian.demo.aoc2018
 
 import org.hypatian.demo.aoc.AoCBase
+import scala.collection.mutable.Map
 
 object Day11 extends AoCBase(11) {
 
   val serial: Int = 4842
 
-  def powerLevel(x: Int, y: Int): Int =
-    (((x + 10) * y + serial) * (x + 10)) / 100 % 10 - 5
+  private[this] val powerLevelCache: Map[(Int, Int, Int), Int] = Map.empty
+  def powerLevel(x: Int, y: Int, s: Int): Int =
+    powerLevelCache.getOrElseUpdate((x, y, s), s match {
+      case 1 => (((x + 10) * y + serial) * (x + 10)) / 100 % 10 - 5
+      case 2 =>
+        powerLevel(x, y,     1) + powerLevel(x + 1, y,     1) +
+        powerLevel(x, y + 1, 1) + powerLevel(x + 1, y + 1, 1)
+      case n =>
+        powerLevel(x + n - 1, y, 1) + powerLevel(x, y + n - 1, 1) +
+        powerLevel(x, y, n - 1)     + powerLevel(x + 1, y + 1, n - 1) -
+        powerLevel(x + 1, y + 1, n - 2)
+    })
 
-  def gridLevel(x: Int, y: Int, s: Int): Int =
-    Seq.tabulate(s, s)((dx, dy) => powerLevel(x + dx, y + dy)).flatten.sum
+  def problem1() =
+    print(
+      (for { x <- 1 to 298; y <- 1 to 298 } yield (x, y)).
+        maxBy(powerLevel(_, _, 3)))
 
-  def maxLevelAtSize(s: Int): (Int, Int) =
-    (for { x <- 1 to (301 - s); y <- 1 to (301 - s) } yield (x, y)).
-      maxBy(gridLevel(_, _, s))
-
-  def maxLevel: (Int, Int, Int) =
-    Stream.range(1, 300).map({ s =>
-      val (x, y) = maxLevelAtSize(s)
-      (x, y, s)
-    }).sliding(2).collectFirst({
-      case Stream((x1, y1, s1), (x2, y2, s2))
-          if gridLevel(x2, y2, s2) < gridLevel(x1, y1, s1) =>
-        (x1, y1, s1)
-    }).get
-
-  def problem1() = {
-    print(maxLevelAtSize(3))
-  }
-
-  def problem2() = {
-    print(maxLevel)
-  }
+  def problem2() =
+    print(
+      (for { s <- 1 to 300; x <- 1 to 301 - s; y <- 1 to 301 - s }
+         yield (x,y,s)).
+        maxBy(powerLevel))
 
 }
